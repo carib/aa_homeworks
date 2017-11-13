eb6d23ace1b6d10888711e6cb6cc62b34ac8c1b7
Connor Yanz <connor.yanz@gmail.com>
2017-11-13 13:11:27 -0500

    Initial commit

---
+++ error_handling_funtime/super_useful.rb
@@ -0,0 +1,63 @@
+# PHASE 2
+def convert_to_int(str)
+  begin
+  Integer(str)
+rescue StandardError => e
+    puts "Invalid argument"
+    nil
+  end
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  else 
+    raise StandardError 
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  begin
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue StandardError => e
+    if maybe_fruit == 'coffee'
+      puts "Ok, I'll give you another shot!"
+      retry
+    else
+      raise StandardError.new("I don't like it!")
+    end
+  end
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    raise StandardError.new("You gotta tell me your name!") unless name.length >= 1
+    raise StandardError.new("We barely know each other!") unless yrs_known >= 5
+    raise StandardError.new("I don't even know your hobbies :(") unless fav_pastime.length >= 1
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
---
+++ error_handling_funtime/user_script.rb
@@ -0,0 +1,11 @@
+require_relative 'super_useful'
+
+# puts "'five' == #{convert_to_int('five')}"
+
+# feed_me_a_fruit
+
+sam = BestFriend.new('Billy Jean', 5, 'haute cuisine')
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
