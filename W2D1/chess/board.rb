7dc13dd84dfefd96f3c948eceda3c4c28e60f2d9
Connor Yanz <connor.yanz@gmail.com>
2017-11-13 17:36:46 -0500

    Add board render

--- W2D1/chess/board.rb
+++ W2D1/chess/board.rb
@@ -1,6 +1,6 @@
 class Board
   def initialize
-    @grid = Array.new(8) { Array.new(8) { nil } }
+    @grid = Array.new(8) { Array.new(8) { NullPiece.new } }
     @grid.map!.with_index do |row, i|
       [0, 1, 6, 7].include?(i) ? row.map! { |pos| pos = Piece.new } : row
     end
@@ -28,11 +28,39 @@ class Board
     diff.all? { |el| el < 9 && el > -1 }
   end
   
-  
+  def display(cursor_pos)
+    str = ""
+    @grid.each_with_index do |row, i|
+      row.each_with_index do |pos, j|
+        if pos.is_a?(NullPiece)
+          [i,j] == cursor_pos ? str += pos.to_s(:red) : str += pos.to_s(:light_blue)
+        else
+          [i,j] == cursor_pos ? str += pos.to_s(:red) : str += pos.to_s(:light_blue)
+        end
+        # str[-4..-1].colorize(:red) if [i, j] == cursor_pos
+      end
+      str += "\n"
+    end
+    str
+  end
 end
 
 class Piece
+  
+  def initialize
+  end
+  
+  def to_s(color)
+    var = ":p"
+    "|#{var.colorize(color)}|"
+  end
 end
 
 class NullPiece < Piece
+  
+  def to_s(color)
+    var = "__"
+    "|#{var.colorize(:background => color)}|"
+  end
+  
 end
--- W2D1/chess/display.rb
+++ W2D1/chess/display.rb
@@ -1,13 +1,18 @@
 require 'colorize'
 require_relative 'cursor' #==> initializes with @cursor_pos
 require_relative 'board'
+require 'byebug'
 
 
 class Display
-
+  attr_accessor :cursor
   def initialize(board = Board.new)
     @cursor = Cursor.new([0,0], board)
-    
+    @board = board
   end
-
+  
+  def render
+    # debugger
+    puts @board.display(@cursor.cursor_pos)
+  end
 end
