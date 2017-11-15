deaeef0d598571f7d18b997086891014600cf979
carib and monica <caribfguerra@gmail.com>
2017-11-15 18:15:22 -0500

    Finished Hanoi spec

--- W2D3/TDD/lib/hanoi.rb
+++ W2D3/TDD/lib/hanoi.rb
@@ -1,7 +1,37 @@
 class HanoiGame 
   attr_reader :towers 
    
-  def initialize   
-    @towers = [[3,2,1],[],[]]
+  def initialize(towers = [[3,2,1],[],[]])   
+    @towers = towers
   end 
+  
+  def get_move 
+    puts "Make a move"
+    move = gets.chomp 
+    move = move.scan(/\d/).map(&:to_i)
+  end 
+  
+  def valid_move?(move)
+    from, to = move 
+    return false if self.towers[from].empty?
+    return true if self.towers[to].empty?
+    return self.towers[from].last < self.towers[to].last     
+  end 
+  
+  def move(from_to)
+    if valid_move?(from_to)
+      from, to = from_to
+      self.towers[to].push(self.towers[from].pop)
+    else
+      puts "Not a valid move."
+      get_move
+    end
+  end 
+  
+  def won?
+    towers.drop(1).any? { |t| t.count == 3 }
+  end
+  
+  private 
+  attr_writer :towers
 end 
--- W2D3/TDD/spec/hanoi_spec.rb
+++ W2D3/TDD/spec/hanoi_spec.rb
@@ -12,20 +12,53 @@ describe HanoiGame do
       expect(hanoi.towers.first).to eq(sorted)
     end
   end
+    
+  describe '#valid_move?' do 
+    let(:moved_game) { HanoiGame.new([[3, 2], [], [1]]) }
+    context "move is valid" do 
+      let(:valid) {[0,1]}
+      it 'should return a valid move' do
+        expect(moved_game.valid_move?(valid)).to be(true)
+      end
+    end 
+    
+    context "move is invalid" do 
+      let(:invalid_empty) {[1, 0]}
+      let(:invalid_size) {[0, 2]}
+      
+      it 'should check if from tower is empty' do
+        expect(moved_game.valid_move?(invalid_empty)).to be(false)
+      end
+      
+      it 'should check if last element in from tower is smaller than that in to tower' do
+        expect(moved_game.valid_move?(invalid_size)).to be(false)
+      end
+    end 
+  end 
   
-  describe '#get_move' do
-    before(:example) do
-      @move = hanoi.get_move
+  describe "#move" do
+      it "should check if move is valid" do 
+        expect(hanoi).to receive(:valid_move?).with([0,1])
+        hanoi.move([0,1])
+      end 
+
+      it 'should move last element from tower to to_tower' do
+        hanoi.move([0,1])
+        expect(hanoi.towers).to eq([[3, 2], [1], []])
+      end 
+
+      it 'should ask for another move if move is invalid' do
+        expect(hanoi).to receive(:get_move)
+        hanoi.move([1, 0])
+      end 
+  end 
+  
+  describe "#won?" do 
+    let(:won_game) { HanoiGame.new([[], [], [3, 2, 1]]) }
+    it 'should check if not original tower is length three' do
+      expect(won_game.won?).to be_truthy
     end
-    
-    it 'should return an array' do
-      expect(@move).to be_a(Array)
-    end
-    
-    it 'should return a valid move' do
-      expect(hanoi).to receive(:valid_move?).and_return(true)
-    end
-  end
+  end 
 end 
 
 
@@ -33,12 +66,12 @@ end
 
 DONE
 - @towers 
+- get_move 
+- valid_move? 
 TODO:
 - move 
 - won? 
 - play 
-- get_move 
-- valid_move? 
 
 
 =end 
