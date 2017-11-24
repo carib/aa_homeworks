516668a50466d586e757cc2439f29c223a176774
Carib <caribfguerra@gmail.com>
2017-11-24 16:25:57 -0500

    Complete #initialize

--- W3D5/active_record_lite_i/lib/00_attr_accessor_object.rb
+++ W3D5/active_record_lite_i/lib/00_attr_accessor_object.rb
@@ -1,5 +1,8 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) { instance_variable_get("@#{name}") }
+      define_method("#{name}=") { |obj| instance_variable_set("@#{name}", obj) }
+    end
   end
 end
--- W3D5/active_record_lite_i/lib/01_sql_object.rb
+++ W3D5/active_record_lite_i/lib/01_sql_object.rb
@@ -4,39 +4,60 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+
+
   def self.columns
-    # ...
+    @columns ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    @columns.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |col|
+      define_method(col) { self.attributes[col] }
+      define_method("#{col}=") { |obj| self.attributes[col] = obj }
+    end
   end
 
-  def self.table_name=(table_name)
+  def self.table_name=(table_name = self.table_name)
     # ...
   end
 
   def self.table_name
-    # ...
+    "#{self.name.downcase}s"
   end
 
   def self.all
-    # ...
+    DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |params| self.new(params) }
   end
 
   def self.find(id)
-    # ...
+    # self.all.find { |obj| obj.id == id }
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, val|
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name)
+      self.send("#{attr_name}=", val)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
