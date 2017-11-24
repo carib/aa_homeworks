c4f8dbc51163b3a8e6bf51bc5782fea9783e728b
Carib <caribfguerra@gmail.com>
2017-11-24 18:13:36 -0500

    Add W3D5

--- W3D5/W3D5/active_record_lite_i/lib/01_sql_object.rb
+++ W3D5/W3D5/active_record_lite_i/lib/01_sql_object.rb
@@ -4,8 +4,6 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
-
-
   def self.columns
     @columns ||= DBConnection.execute2(<<-SQL)
       SELECT
@@ -25,7 +23,7 @@ class SQLObject
   end
 
   def self.table_name=(table_name = self.table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
@@ -33,12 +31,13 @@ class SQLObject
   end
 
   def self.all
-    DBConnection.execute(<<-SQL)
+    objects = DBConnection.execute(<<-SQL)
       SELECT
         *
       FROM
         #{table_name}
     SQL
+    self.parse_all(objects)
   end
 
   def self.parse_all(results)
@@ -46,11 +45,22 @@ class SQLObject
   end
 
   def self.find(id)
-    # self.all.find { |obj| obj.id == id }
+    object = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = #{id}
+      LIMIT
+        1
+    SQL
+    self.parse_all(object).first
   end
 
   def initialize(params = {})
     params.each do |attr_name, val|
+      attr_name = attr_name.to_sym
       raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name)
       self.send("#{attr_name}=", val)
     end
@@ -61,15 +71,34 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |col| self.send(col) }
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(', ')
+    question_marks = ['?'] * attribute_values.size
+
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks.join(', ')})
+    SQL
+    DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_line = self.class.columns.map { |col| "#{col} = ?" }.join(', ')
+    # p attribute_values
+    DBConnection.execute(<<-SQL *attribute_values)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = #{self.id}
+    SQL
   end
 
   def save
