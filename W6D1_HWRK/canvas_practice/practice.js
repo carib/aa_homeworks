51f5b083ad668b58d225279758b6c7fd421878fa
Carib Rivera-Guerra <caribfguerra@gmail.com>
2017-12-10 16:31:24 -0500

    Complete homework

--- W6D1_HWRK/canvas_practice/practice.js
+++ W6D1_HWRK/canvas_practice/practice.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
+  var canvas = document.getElementById('mycanvas');
+  var ctx = canvas.getContext('2d');
 
+  // Fill color
+  ctx.fillStyle = 'green';
+  // Dimensions: (x, y, width, height)
+  ctx.fillRect(10, 10, 100, 100);
+
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'blue';
+  // (x, y, radius, startAngle, endAngle, anticlockwise)
+  ctx.arc(250, 75, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = 'pink';
+  ctx.fill();
 });
