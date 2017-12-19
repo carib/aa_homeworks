3658e426c9008954ca7099e3fa61d3c0390a1d34
Carib Rivera-Guerra <caribfguerra@gmail.com>
2017-12-18 19:14:41 -0500

    Complete middleware homework

--- W7D2_HWRK/middleware/frontend/todo_redux.jsx
+++ W7D2_HWRK/middleware/frontend/todo_redux.jsx
@@ -7,7 +7,7 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
